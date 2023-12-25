fe1409147d17a4b57ac2d71de96e4596bdb21552
ggnohope <lamhoa9a1thcsttlp@gmail.com>
2023-12-25 18:54:58 +0700

    add find_exit()

--- agent_brain.py
+++
@@ -1,8 +0,0 @@
-class AgentBrain:
-    def __init__(self, current_cell, grid_cells):
-        self.current_cell = current_cell
-        self.grid_cells = grid_cells
-        self.action_list = []
-        self.action_cells = {}
-        self.found_exit = False
-        self.remain_cells = []
